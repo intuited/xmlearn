70e817f91c9bfc342744a3b9c55b56bda5c6c53a
Ted Tibbetts <intuited@gmail.com>
2010-07-17 19:12:53 -0300

    add option to override object properties when calling `dump`
    also make docstring improvements

--- __init__.py
+++ __init__.py
@@ -6,12 +6,60 @@ class LXML_Dumper(object):
     Attributes:
         maxdepth: the maximum recursion level.
         width: the width at which text will be wrapped.
+        rules: either a string matching one of the keys of rulesets,
+                 or a Mapping containing such a ruleset.
+
+    For now, this function is just useful for sussing out the shape of the XML.
+
+    Setup:
+        >>> import lxml.etree as etree
+        >>> book = etree.parse('protocol.xml').getroot()
+        >>> dumper = LXML_Dumper(maxdepth=2)
+        >>> dumper.dump(book)
+        >>> [dumper.dump(c, maxdepth=1, rules='full')
+        ...  for c in book.iterfind('chapter')]
+        General protocol syntax (/book/chapter[1]):
+            [title] (/book/chapter[1]/title): General protocol syntax
+            Requests (/book/chapter[1]/section[1]):
+            Responses (/book/chapter[1]/section[2]):
+            Command lists (/book/chapter[1]/section[3]):
+            Ranges (/book/chapter[1]/section[4]):
+        Command reference (/book/chapter[2]):
+            [title] (/book/chapter[2]/title): Command reference
+            [note] (/book/chapter[2]/note):
+            Querying MPD's status (/book/chapter[2]/section[1]):
+            Playback options (/book/chapter[2]/section[2]):
+            Controlling playback (/book/chapter[2]/section[3]):
+            The current playlist (/book/chapter[2]/section[4]):
+            Stored playlists (/book/chapter[2]/section[5]):
+            The music database (/book/chapter[2]/section[6]):
+            Stickers (/book/chapter[2]/section[7]):
+            Connection settings (/book/chapter[2]/section[8]):
+            Audio output devices (/book/chapter[2]/section[9]):
+            Reflection (/book/chapter[2]/section[10]):
+
     """
 
+    # Each ruleset maps tags to kwargs for `format_element`.
+    rulesets = {'book': {'section': {'with_text': False},
+                         'para': {'linebreak': True},
+                         None: {'recurse': True}},
+                'full': {None: {'recurse': True}}}
+
+    maxdepth = None
+    width = 80
+    rules = rulesets['book']
+
     def __init__(self, **kwargs):
+        """Initialize object attributes documented in the class docstring."""
         self.__dict__.update(kwargs)
 
     def relwrap(self, *args, **kwargs):
+        """Wrap the text with an indent relative to the `depth` keyword arg.
+
+        The text is indented 4 spaces for each level of depth.
+        Wrapped lines are indented an extra 2 spaces.
+        """
         from textwrap import wrap
         depth = kwargs.pop('depth', 0)
         wrap_kwargs = dict({'initial_indent': '    ' * depth,
@@ -36,22 +84,21 @@ class LXML_Dumper(object):
             fmt = "{0} ({1}): {2}" if with_text else "{0} ({1})"
             return self.relwrap(fmt.format(title, path, eltext), depth=depth)
 
-    # Each ruleset maps tags to kwargs for `format_element`.
-    rulesets = {'book': {'section': {'with_text': False},
-                         'para': {'linebreak': True},
-                         None: {'recurse': True}},
-                'full': {None: {'recurse': True}}}
-
-    def dump(self, element, rules=rulesets['book'], depth=0):
+    def dump(self, element, **kwargs):
         """Dump `element` according to `rules`.
         
         Keyword arguments:
-            rules: either a string matching one of the keys of rulesets,
-                     or a Mapping containing such a ruleset.
-            depth: the initial depth of the dump.
+            depth: the initial depth of the dump.  Normally this will be 0.
+
+            Additionally, the `maxdepth` and `rules` object keyword arguments
+              can be overridden.
         """
         from copy import copy
         from collections import Mapping
+
+        depth = kwargs.pop('depth', 0)
+        maxdepth, rules = (kwargs.pop(a, getattr(self, a))
+                           for a in ('maxdepth', 'rules'))
 
         if isinstance(rules, Mapping):
             pass
@@ -73,19 +120,8 @@ class LXML_Dumper(object):
             recurse = kwargs.pop('recurse', False)
             print self.format_element(element, depth, **kwargs)
             if recurse:
-                if (hasattr(self, 'maxdepth') and self.maxdepth >= depth
-                    or not hasattr(self, 'maxdepth')
-                ):
+                if (maxdepth is None or maxdepth > depth):
                     for child in element.getchildren():
                         _dump(child, depth + 1)
 
         _dump(element, depth=depth)
-                    
-##--              if element.tag == 'section':
-##--                  print self.format_element(element, depth, with_text=False)
-##--              elif element.tag == 'para':
-##--                  print self.format_element(element, depth, linebreak=True)
-##--              else:
-##--                  print self.format_element(element, depth)
-##--                  for child in element.getchildren():
-##--                      _dump(child, depth + 1)
